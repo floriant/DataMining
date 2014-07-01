765fad31494d71922d66ecf9e5766b4ead6c3ebe
Florian 'Panade' Tatzel <panade@playerscene.de>
2014-07-02 00:24:45 +0200

    Added TextFile Generation, Cleaned Code Up, Comented Important Parts and fixed a Bug with matrix generation

--- Versuch_5/Program/newsfeatured222_2.py
+++ Versuch_5/Program/newsfeatured222_2.py
@@ -1,4 +1,6 @@
 import pprint as pp
+
+# Example Data for Testing
 allwords = {'aided':3, 'actionfor': 2, 'ambitionsthe':2, 'reboot':4, 'four':8, 'accused': 10, 'into': 3, 'racism': 4, 'debate':3}
 articlewords = {}
 articlewords[0] = {'accused': 1, 'into': 1, 'racism': 2, 'debate': 1}
@@ -6,76 +8,110 @@ articlewords[1] = {'accused': 1, 'aided': 1, 'actionfor': 2, 'ambitionsthe': 1, 
 articlewords[2] = {'aided':1, 'ambitionsthe':1, 'reboot':1, 'four':2, 'into': 1, 'debate':1}
 articlewords[3] = { 'reboot':1, 'four':2, 'accused': 2}
 articlewords[4] = {'aided':1, 'actionfor': 1, 'ambitionsthe':1, 'reboot':1, 'four':2, 'accused': 2, 'into': 1, 'racism': 2, 'debate':1}
-print allwords
 
+# Global Variables used for article Word Matrix
 wordvec = {}
 wordInArt = {}
 
-
+# makematrix Function to write the wordvec and wordInArt Global Variables
+#
+# Params
+# allw: Dict - Dictionary that contains all Words with their overall count.
+# articlewords: Dict - Dictionary that contains numeric Keys for each Article which contains Words as Keys and WordCount as Value
+#
+# Note:
+# The wordvec and wordInArt results, will be written as Dict.
+# This way no Information about the Words gets lost.
 def makematrix(allw, articlew):
-    # Removing Words with less tan 4 occourences
+
+    # Removing words that have a overall Wordcount >= 4
     trimmedV = {}
     for word in allwords:
-        print(allwords[word])
         if allwords[word] >= 4:
             trimmedV[word] = allwords[word]
-    print(trimmedV)
 
-    # Removing Words with less than 60% occurence
+    # Removing words that appear > 60% in all Articles
+    #
+    # artP: Percent per Article occurence
+    # percentage: Percentage counting Value
+    # trimmedPercent: Copy of trimmedV since Python doesnt allow changing Dicts in a Loop
     artP = (100 / len(articlewords))
     percentage = 0
     trimmedPercent = trimmedV.copy()
-    print(artP)
-    print("--------------AllWords V")
-    print(trimmedV)
-    print("--------------//")
     for wordV in trimmedV:
-        print("--------------Current Word V")
-        print(wordV)
-        print("--------------//")
         for article in articlewords:
-            print("--------------Current Article")
-            print(article)
-            print(articlewords[article])
-            print("--------------//")
-            #for aword in articlewords[article]:
-            #    print(aword)
-            print("--------------Trimmed V word V and haskey?")
-            print(wordV)
-            print(articlewords[article].has_key(wordV))
-            print("--------------//")
             if articlewords[article].has_key(wordV):
                 percentage += artP
-            print("#####################################################")
         if percentage < 60:
-            print percentage
             trimmedPercent.pop(wordV)
         percentage = 0
+
+    # Debugging output for the two Word Reduction Steps
+    print('###########################################')
+    print('############## >= 4 Check #################')
+    print('###########################################')
     print(trimmedV)
+    print('###########################################')
+    print('############## > 60% Check ################')
+    print('###########################################')
     print(trimmedPercent)
 
-    #Wir verwenden anstelle listen Dictionarys
-    #Wenn man Listen uebergibt verliert man die reihenfolge der worte
-    #durch dictionarys kann man jederzeit eine semantische zuordnung vornehmen.
-
-    print('######################################################')
-    print('######################################################')
-    print('######################################################')
-    print('######################################################')
-
     # Create Article/Word Matrix
+    #
+    # We Loop trough the articlewords for vector i
+    # We Loop trough trimmedPercent for vector j
     awMatrix = {}
     for article in articlewords:
-        print(article)
         awMatrix[article] = {}
         for wordAW in trimmedPercent:
-            #print(wordAW)
-            #print(allwords[wordAW])
-            awMatrix[article][wordAW] = allwords[wordAW]
+            if wordAW in articlewords[article]:
+                awMatrix[article][wordAW] = articlewords[article][wordAW]
+            else:
+                awMatrix[article][wordAW] = 0
+
+    # Printing the awMatrix for Debugging Purposes
+    print('###########################################')
+    print('######## The Article/Word Matrix ##########')
+    print('###########################################')
     pp.pprint(awMatrix)
 
+    # Writing the wordvec and wordInArt Variables
+    # Note: This should give us an reference, so no Additional Space is wasted in RAM
     wordvec = trimmedPercent
     wordInArt = awMatrix
+
+    # Creating Text File with Matrix
+    file = open('../res/awMatrix.txt', 'w')
+    wordvecText = ''
+    # First Line consists of wordvec
+    for i, txtWord in enumerate(trimmedPercent):
+        if i != len(trimmedPercent)-1:
+            wordvecText += txtWord + ','
+        else:
+            wordvecText += txtWord + '\n'
+    print('###########################################')
+    print('###### Text to Write from wordvec #########')
+    print('###########################################')
+    print wordvecText
+
+    # Creating the Data from wordInArt Matrix
+    wordInArtText = ''
+    for txtArticle in awMatrix:
+        for i, txtData in enumerate(awMatrix[txtArticle]):
+            if i != len(awMatrix[txtArticle])-1:
+                wordInArtText += str(awMatrix[txtArticle][txtData]) + ','
+            else:
+                wordInArtText += str(awMatrix[txtArticle][txtData]) + '\n'
+    print('###########################################')
+    print('###### Text to Write from wordInArt #######')
+    print('###########################################')
+    print wordInArtText
+
+    # Writing to File
+    file.write(wordvecText)
+    file.write(wordInArtText)
+    file.close()
+
 
 makematrix(allwords, articlewords)
 print("end")
