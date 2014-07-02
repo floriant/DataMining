17ad7ba3d506771e8b2a8ac9878300c718cf713e
Marc Walter <walter.marc@outlook.com>
2014-07-02 19:37:07 +0200

    bug in 2.2.4 removed

--- Versuch_5/Program/newsfeatures224.py
+++ Versuch_5/Program/newsfeatures224.py
@@ -2,6 +2,8 @@
 import pprint as pp
 import numpy as np
 
+debug = False
+
 #A and B are of type numpy.matrix
 #returns the summed euclidean distance of the passed matrices
 def cost(A, B):
@@ -56,26 +58,26 @@ def nnmf(A, m, it):
 
     #step 3+4:
     #initialize matrices H and W
-    H = np.random.randint(0, 6, (m, c))
+    H = np.random.randint(1, 7, (m, c)) #0 needs to be excluded
     H = np.matrix(H)
 
-    W = np.random.randint(0, 6, (r, m))
+    W = np.random.randint(1, 7, (r, m)) #0 needs to be excluded
     W = np.matrix(W)
-    print "shape of H: ", H.shape, "\nshape of W: ", W.shape
 
-    pp.pprint({'H': H, 'W': W})
+    if debug:
+        print "shape of H: ", H.shape, "\nshape of W: ", W.shape
+        pp.pprint({'H': H, 'W': W})
 
     #step 5:
     while it > 0:
         #calculate current product of H and W
-
         #a)
         B = W * H
-        print "shape of B: ", B.shape
-
         k = cost(A, B)
-        pp.pprint({'A': A, 'B': B})
-        print "cost: %d" % k
+
+        if debug:
+            pp.pprint({'A': A, 'B': B})
+            print "cost: %d" % k
 
         #break if desired matrix and factorized matrix are very similar
         if k < 5:
@@ -84,72 +86,38 @@ def nnmf(A, m, it):
         #b)
         #recalculate
         # Hij = Hij * (W_transposed * A)ij / (W_transposed * W * H)ij
+        temp1 = np.array(W.T * A)
+        temp2 = np.array(W.T * W * H)
+        H = np.matrix( np.array(H) * np.true_divide(temp1, temp2) ) #normal divide floors the results
 
-        eins = W.T * A
-        print "shape of eins: ", eins.shape
-
-        zwei = W.T * W * H
-        print "shape of zwei: ", zwei.shape
-
-        drei = np.array(eins) / np.array(zwei)
-        print "shape of drei: ", drei.shape
-
-        H_arr = np.array(H) * drei
-
-        print "HArray:"
-        pp.pprint(H_arr)
-
-        H = np.matrix(H_arr)
-        print "shape of H: ", H.shape, "H:"
-        pp.pprint(H)
+        if debug:
+            print "shape of H: ", H.shape, "H:"
+            pp.pprint(H)
 
         #c)
         #Wij = Wij * (A * H_transposed)ij / (W * H * H_transposed)ij
+        #normal divide floors the results
+        nextW = np.array(W) * np.true_divide(np.array(A * H.T), np.array(W * H * H.T))
+        W = np.matrix(nextW)
 
-        nextW = np.array(W) * np.array(A * H.T) / np.array(W * H * H.T)
 
-        W = nextW
         it -= 1
-        print "current values: (%d more iterations)\n" % it
-        print "W:\n", W, "\nH:\n", H
-        print "-"*64
+        if debug:
+            print "current values: (%d more iterations)\n" % it
+            print "W:\n", W, "\nH:\n", H
+            print "-"*64
 
-        pass
-
-
-    print "r: %d, c: %d" % (r, c)
     #return {'H': H, 'W': W}
     return W, H
 
 
 if __name__ == "__main__":
-    #TODO Exercise 2.2.3: check if Matrix contains no row only of zeros
     A = np.matrix([[1, 2, 3], [0, 0, 2], [2, 0, 1], [2, 0, 0]])
 
-    A *= 1.0
     result = nnmf(A, 2, 3)
     W, H = result
 
-    print "W:\n", W
-    print "H:\n", H
+    print "\n", "-"*64, "\nresult:\nW:\n", W, "\nH:\n", H
 
-    #print "k = " + str(cost(A,B))
-    """try:
-        nnmf(A, 2, 4)
-    except Exception, e:
-        print "Exception: ", e
-        pass
-
-    print "\n", '#' * 80, "\n"
-
-    "" "
-    try:
-        nnmf(A, 2, 4)
-    except Exception, e:
-        print "Exception: ", e
-        pass
-    #print "nnmf result:"
-    #pp.pprint(result)
-    """
 
     pass
