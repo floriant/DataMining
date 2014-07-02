c768aad6ae0dc99715d70738c8ca1f17a20c4a3e
Florian 'Panade' Tatzel <panade@playerscene.de>
2014-07-02 21:57:39 +0200

    Fixed the Variable Call, Added Pop from articletitles and commented Test Data

--- Versuch_5/Program/newsfeatured222_2.py
+++ Versuch_5/Program/newsfeatured222_2.py
@@ -2,14 +2,15 @@ import pprint as pp
 import numpy as np
 
 # Example Data for Testing
-allwords = {'aided':3, 'actionfor': 3, 'ambitionsthe':2, 'reboot':4, 'four':8, 'accused': 10, 'into': 3, 'racism': 4, 'debate':3}
-articlewords = {}
-articlewords[0] = {'accused': 1, 'into': 1, 'racism': 2, 'debate': 1}
-articlewords[1] = {'accused': 1, 'aided': 1, 'actionfor': 2, 'ambitionsthe': 1, 'reboot': 1, 'four': 2}
-articlewords[2] = {'aided':1, 'ambitionsthe':1, 'reboot':1, 'four':2, 'into': 1, 'debate':1}
-articlewords[3] = { 'reboot':1, 'four':2, 'accused': 2}
-articlewords[4] = {'aided':1, 'actionfor': 1, 'ambitionsthe':1, 'reboot':1, 'four':2, 'accused': 2, 'into': 1, 'racism': 2, 'debate':1}
-articlewords[5] = {'actionfor': 1}
+#allwords = {'aided':3, 'actionfor': 3, 'ambitionsthe':2, 'reboot':4, 'four':8, 'accused': 10, 'into': 3, 'racism': 4, 'debate':3}
+#articlewords = {}
+#articlewords[0] = {'accused': 1, 'into': 1, 'racism': 2, 'debate': 1}
+#articlewords[1] = {'accused': 1, 'aided': 1, 'actionfor': 2, 'ambitionsthe': 1, 'reboot': 1, 'four': 2}
+#articlewords[2] = {'aided':1, 'ambitionsthe':1, 'reboot':1, 'four':2, 'into': 1, 'debate':1}
+#articlewords[3] = { 'reboot':1, 'four':2, 'accused': 2}
+#articlewords[4] = {'aided':1, 'actionfor': 1, 'ambitionsthe':1, 'reboot':1, 'four':2, 'accused': 2, 'into': 1, 'racism': 2, 'debate':1}
+#articlewords[5] = {'actionfor': 1}
+#articletitles = ['A', 'B', 'C', 'D', 'E', 'F']
 
 # Global Variables used for article Word Matrix
 wordvec = {}
@@ -28,24 +29,25 @@ def makematrix(allw, articlew):
     # Declaring Global Vars
     global wordvec
     global wordInArt
+    global articletitles
 
     # Removing words that have a overall Wordcount >= 4
     trimmedV = {}
-    for word in allwords:
-        if allwords[word] >= 4:
-            trimmedV[word] = allwords[word]
+    for word in allw:
+        if allw[word] >= 4:
+            trimmedV[word] = allw[word]
 
     # Removing words that appear > 60% in all Articles
     #
     # artP: Percent per Article occurence
     # percentage: Percentage counting Value
     # trimmedPercent: Copy of trimmedV since Python doesnt allow changing Dicts in a Loop
-    artP = (100 / len(articlewords))
+    artP = (100 / len(articlew))
     percentage = 0
     trimmedPercent = trimmedV.copy()
     for wordV in trimmedV:
-        for article in articlewords:
-            if articlewords[article].has_key(wordV):
+        for article in articlew:
+            if articlew[article].has_key(wordV):
                 percentage += artP
         if percentage < 60:
             trimmedPercent.pop(wordV)
@@ -67,13 +69,13 @@ def makematrix(allw, articlew):
     # We Loop trough trimmedPercent for vector j
     awMatrix = {}
     valueCount = 0
-    for article in articlewords:
+    for article in articlew:
         awMatrix[article] = {}
         valueCount = 0
         for wordAW in trimmedPercent:
             wordTrashhold = wordAW
-            if wordAW in articlewords[article]:
-                awMatrix[article][wordAW] = articlewords[article][wordAW]
+            if wordAW in articlew[article]:
+                awMatrix[article][wordAW] = articlew[article][wordAW]
                 valueCount = valueCount + 1
             else:
                 awMatrix[article][wordAW] = 0
@@ -82,6 +84,8 @@ def makematrix(allw, articlew):
         # then pop the Artice out of the Dict
         if valueCount == 0:
             awMatrix.pop(article)
+            articletitles.pop(article)
+
 
     # Printing the awMatrix for Debugging Purposes
     print('###########################################')
@@ -153,4 +157,5 @@ def transformMatrix(awDict):
 # Testing the Functions
 makematrix(allwords, articlewords)
 awMatrix = transformMatrix(wordInArt)
+print(articletitles)
 print("end")
